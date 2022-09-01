dad7d2d2097df9257442a22fdbbd4cf280c6ef38
gabrielJGS <gabrielftjose@gmail.com>
2022-09-01 14:31:41 -0300

    feat: winston log info route

--- src/routes/calculo.route.ts
+++ src/routes/calculo.route.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
 import calculoMediaRepository from "../repositories/calculo.repository";
+import logger from "../config/logger";
 
 const calculoMediaRoute = Router();
 
@@ -8,9 +9,9 @@ calculoMediaRoute.post("/calculomedia", async (req: Request, res: Response, next
   try {
     const { n1, n2 } = req.body;
     const calculoMedia = await calculoMediaRepository.calcular(n1, n2);
+    logger.info(`n1: ${n1} n2: ${n2} Resultado=${calculoMedia}`);
     res.status(StatusCodes.OK).json(calculoMedia);
   } catch (error) {
-    console.error(error);
     next(error);
   }
 });
