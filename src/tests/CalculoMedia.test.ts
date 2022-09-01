a679c565a01c340dda5c4907bcae3c26a83e64b8
gabrielJGS <gabrielftjose@gmail.com>
2022-09-01 13:44:23 -0300

    test: criacao do teste da classe calculomedia

---
+++ src/tests/CalculoMedia.test.ts
@@ -0,0 +1,22 @@
+import CalculoMedia from "../models/CalculoMedia";
+
+describe("Testa a classe CalculoMedia", () => {
+  test("testa o retorno do calculo da media de 2 numeros", () => {
+    const mock_n1 = 5;
+    const mock_n2 = 4;
+
+    const calculoMedia = new CalculoMedia(mock_n1, mock_n2);
+    const media = calculoMedia.calcular();
+
+    expect(media).toEqual(4.5);
+  });
+
+  test("testa o tratamento de erros no caso de parâmetro inválido", () => {
+    const mock_n1: any = null;
+    const mock_n2 = 2;
+
+    expect(() => {
+      new CalculoMedia(mock_n1, mock_n2);
+    }).toThrow();
+  });
+});
