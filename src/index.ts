2e2f8018034129b62d7dde44d8f45a4cb91c0459
gabrielJGS <gabrielftjose@gmail.com>
2022-09-01 15:53:45 -0300

    feat/fix: cep route e reordenar errorMiddleware

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ require('dotenv').config();
 import express from "express";
 import errorHandler from "./middlewares/error-handler.middleware";
 import calculoRoute from "./routes/calculo.route";
+import cepRoute from "./routes/cep.route";
+
 const { PORT } = process.env || 3333;
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(calculoRoute);
 app.use(errorHandler);
+
+app.use(calculoRoute);
+app.use(cepRoute);
 
 app.listen(PORT, () => {
   console.log(`Aplicação executando na porta ${PORT}`);
